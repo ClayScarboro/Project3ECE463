e5d7cd18f58b33e1a7078e21be8030b904c52de8
ClayScarboro <clayscarbor@gmail.com>
2022-12-12 17:27:03 -0500

    Update sim_proc.h

--- cpp_files/sim_proc.h
+++ cpp_files/sim_proc.h
@@ -6,35 +6,7 @@ using namespace std;
 typedef struct proc_params{
     unsigned long int rob_size;
     unsigned long int iq_size;
-    unsigned long int width;
-	
-	//storing cycle count
-	int cycle;
-	
-	//Pipeline Registers
-	vector<uint32_t> DE;
-	vector<uint32_t> RN;
-	vector<uint32_t> RR;
-	vector<uint32_t> DI;
-	vector<uint32_t> IQ;
-	vector<uint32_t> execute_list;
-	vector<uint32_t> WB;
-	vector<uint32_t> ROB;
-	
-	//vector for instruction lookup
-	vector< vector<uint32_t> > instTable;
-	
-	bool Advance_Cycle();
-	void Retire();
-	void Writeback();
-	void Execute();
-	void Issue();
-	void Dispatch();
-	void RegRead();
-	void Rename();
-	void Decode();
-	bool Fetch(FILE*);
-	
+    unsigned long int width;	
 }proc_params;
 
 
